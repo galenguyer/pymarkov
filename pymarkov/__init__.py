037b175a90251988473844b4c4015ec9900744e3
Galen Guyer <galen@galenguyer.com>
2020-11-19 17:38:40 -0500

    add init file

---
+++ pymarkov/__init__.py
@@ -0,0 +1,8 @@
+class PyMarkov:
+    def __init__(filename: str):
+        pass
+
+
+if __name__ == '__main__':
+    print('This is a library and not meant to be called directly. ' + \
+        'Please import it using "import pymarkov"')
